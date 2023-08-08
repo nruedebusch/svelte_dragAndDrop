fc4c8f83ff7924050e7ef29f1e0debc19d979947
nruedebusch <n.ruedebusch@web.de>
2023-08-08 17:54:19 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { flip } from 'svelte/animate';
+	import { dndzone } from 'svelte-dnd-action';
+
+	const flipDurationMs = 100;
+
+	interface ListItem {
+		id: number;
+		title: string;
+		description: string;
+		tags: string[];
+	}
+
+	let items: ListItem[] = [
+		{
+			id: 1,
+			title: 'Complete Character Design',
+			description: 'Design main enemies for Levels 1-3',
+			tags: ['Design', 'Characters']
+		},
+		{
+			id: 2,
+			title: 'Compose Music for Boss Battle',
+			description: 'Compose epic music for the final boss battle',
+			tags: ['Music', 'Boss Battle']
+		},
+		{
+			id: 3,
+			title: 'Test Multiplayer Network Code',
+			description: 'Ensure the network for online gameplay is stable',
+			tags: ['Programming', 'Multiplayer']
+		}
+	];
+
+	const handleConsider = (evt: CustomEvent<DndEvent<ListItem>>) => {
+		console.log('consider');
+		items = evt.detail.items;
+	};
+
+	const handleFinalize = (evt: CustomEvent<DndEvent<ListItem>>) => {
+		console.log('finalize');
+		items = evt.detail.items;
+	};
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<section
+		use:dndzone={{ items: items, flipDurationMs: flipDurationMs, dropTargetStyle: {} }}
+		on:consider={handleConsider}
+		on:finalize={handleFinalize}
+	>
+		{#each items as item (item.id)}
+			<div class="card card-hover w-96 my-4" animate:flip={{ duration: flipDurationMs }}>
+				<header class="card-header">
+					<h4>{item.title}</h4>
+				</header>
+				<section class="p-4">{item.description}</section>
+				<footer class="card-footer inline-block">
+					{#each item.tags as tag}
+						<span class="chip variant-filled-secondary mr-2">{tag}</span>{/each}
+				</footer>
+			</div>
+		{/each}
+	</section>
+</div>
